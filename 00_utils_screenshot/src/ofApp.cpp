e6fab08beb2966d0d6c8eaaa8525f384f0e03c9b
Lisa Jamhoury <lisajamhoury@gmail.com>
2020-06-07 15:33:33 -0400

    start mc

--- 00_utils_screenshot/src/ofApp.cpp
+++ 00_utils_screenshot/src/ofApp.cpp
@@ -1,5 +1,4 @@
 #include "ofApp.h"
-#include "screenshot.hpp"
 
 //--------------------------------------------------------------
 void ofApp::setup(){
---
+++ 03_muriel_cooper_a/src/ofApp.cpp
@@ -0,0 +1,83 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    ofBackground(223,210,202);
+    
+    ofSetColor(39, 23, 19);
+    ofRectMode(OF_RECTMODE_CENTER);
+    float amt = ofMap(mouseX, 0, ofGetWidth(), 0.7, 1.0);
+    
+    cout << amt << endl;
+    ofDrawRectangle(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()*amt, ofGetHeight()*amt);
+    
+
+
+    screenshot.run();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
