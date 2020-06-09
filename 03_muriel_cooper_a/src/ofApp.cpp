4e41e95a4ff30207b6bc25c90a1211a1499dd0bd
Lisa Jamhoury <lisajamhoury@gmail.com>
2020-06-09 14:24:19 -0400

    muriel in progress

--- 03_muriel_cooper_a/src/ofApp.cpp
+++ 03_muriel_cooper_a/src/ofApp.cpp
@@ -2,6 +2,16 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    font.load("helvetica-bold.ttf", 72, true, true, true);
+    font.setLetterSpacing(0.83);
+    debugImg.load("images/smpImgs/03_muriel_cooper_og.jpg");
+
+    squareClr = ofColor(39, 23, 19);
+    borderClr = ofColor(223,210,202);
+    typeClr3 = ofColor(54, 20, 65); // purple
+    typeClr2 = ofColor(1, 74, 24); // green
+    typeClr1 = ofColor(205, 43, 22); // red
+
 
 }
 
@@ -13,16 +23,130 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
     
-    ofBackground(223,210,202);
+    //    float amt = ofMap(mouseX, 0, ofGetWidth(), 0.7, 1.0);
+    //    float numRows = ofMap(mouseX, 0, ofGetWidth(), 1.0, 20.0);
+    //    float ls = ofMap(mouseX, 0, ofGetWidth(), 0.0, 2.0);
+//    float yOff = ofMap(mouseX, 0, ofGetWidth(), 0, 300.0);
     
-    ofSetColor(39, 23, 19);
-    ofRectMode(OF_RECTMODE_CENTER);
-    float amt = ofMap(mouseX, 0, ofGetWidth(), 0.7, 1.0);
-    
-    cout << amt << endl;
-    ofDrawRectangle(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()*amt, ofGetHeight()*amt);
+    float lh = ofMap(mouseX, 0, ofGetWidth(), 0.0, 2.0);
+    font.setLineHeight(lh);
+
+    ofBackground(squareClr);
+//    ofBackground(255);
     
 
+    
+//    ofTranslate(300,300);
+    
+    // back ground image
+//    ofPushMatrix();
+//    ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
+//    ofRotateDeg(45);
+//
+//    debugImg.draw(-ofGetWidth()/2, -ofGetHeight()/2, ofGetWidth(), ofGetHeight());
+//    ofPopMatrix();
+    
+    
+    // create grid
+    
+    ofSetColor(0, 255, 0);
+    ofNoFill();
+    
+    float numRows = 10.0;
+    float rectW = ofGetWidth()/numRows;
+
+    
+    // columns
+    for (int i = 0; i < numRows; i++) {
+        ofDrawRectangle(i*rectW, 0, rectW, ofGetHeight());
+    }
+    
+    // rows
+    for (int i = 0; i < numRows; i++) {
+        float rectW = ofGetWidth()/numRows;
+        ofDrawRectangle(0, i*rectW, ofGetWidth(), rectW);
+    }
+    
+    
+    float yOff = 6.375;
+//    float trans = ofMap(mouseX, 0, ofGetWidth(), 0, 255);
+    float trans = 255;
+    
+    ofEnableBlendMode(OF_BLENDMODE_ADD);
+    
+    ofSetColor(typeClr3, trans);
+    
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
+    ofRotateDeg(180);
+    font.drawString("MESSAGES", 326-ofGetWidth()/2, 556-ofGetHeight()/2+yOff);
+    font.drawString("MEANS", 579-ofGetWidth()/2, 462-ofGetHeight()/2+yOff);
+    
+    font.drawString("MESSAGES", -38-ofGetWidth()/2, 394-ofGetHeight()/2+yOff);
+    font.drawString("MEANS", 19-ofGetWidth()/2, 302-ofGetHeight()/2+yOff);
+    ofPopMatrix();
+    
+    ofSetColor(typeClr2, trans);
+
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
+    ofRotateDeg(90);
+    font.drawString("MESSAGES", 326-ofGetWidth()/2, 556-ofGetHeight()/2+yOff);
+    font.drawString("MEANS", 579-ofGetWidth()/2, 462-ofGetHeight()/2+yOff);
+
+    font.drawString("MESSAGES", -38-ofGetWidth()/2, 394-ofGetHeight()/2+yOff);
+    font.drawString("MEANS", 19-ofGetWidth()/2, 302-ofGetHeight()/2+yOff);
+    ofPopMatrix();
+    
+    ofSetColor(typeClr1, trans);
+//    font.drawString("MESSAGES", 4.0* rectW, 7.0*rectW);
+    font.drawString("MESSAGES", 326, 556+yOff);
+    font.drawString("MEANS", 579, 462+yOff);
+    ofFill();
+
+    font.drawString("MESSAGES", -38, 394+yOff);
+    font.drawString("MEANS", 19, 302+yOff);
+
+    ofDisableBlendMode();
+
+
+    
+
+
+//    cout << mouseX << ", " << mouseY << endl;
+    
+    
+//    vector < ofPath > paths = font.getStringAsPoints("MESSAGES");
+
+//    for (int i = 0; i < paths.size(); i++){
+//        paths[i].setPolyWindingMode(OF_POLY_WINDING_ODD);
+//        vector < ofPolyline > lines = paths[i].getOutline();
+//        for (int j = 0; j < lines.size(); j++){
+//
+//            lines[j].setClosed(true);
+//            lines[j] = lines[j].getResampledBySpacing(3);
+//
+//            for (int k = 0; k < lines[j].size(); k++){
+//                lines[j][k].x += mouseX * sin(lines[j][k].y * 0.04 + ofGetElapsedTimef() + i);
+//            }
+//
+//            lines[j].draw();
+//
+//        }
+//        paths[i].close();
+//        paths[i].simplify();
+//        paths[i].draw();
+//    }
+//
+    
+    
+    // build frame
+     ofSetColor(borderClr);
+     float borderW = ofGetWidth() * (2.0/30.0);
+     ofDrawRectangle(0, 0, ofGetWidth(), borderW);
+     ofDrawRectangle(0, ofGetHeight()-borderW, ofGetWidth(), ofGetHeight());
+     ofDrawRectangle(0, borderW, borderW, ofGetHeight()-borderW);
+     ofDrawRectangle(ofGetWidth()-borderW, borderW, borderW, ofGetHeight()-borderW);
 
     screenshot.run();
 }
--- 03_muriel_cooper_a/src/ofApp.h
+++ 03_muriel_cooper_a/src/ofApp.h
@@ -4,24 +4,34 @@
 #include "screenshot.hpp"
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-        
-        // create an instance of screenshot util
-        screenshotUtil screenshot;
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    // create an instance of screenshot util
+    screenshotUtil screenshot;
+    
+    ofTrueTypeFont font;
+    ofColor squareClr;
+    ofColor borderClr;
+    ofColor typeClr1;
+    ofColor typeClr2;
+    ofColor typeClr3;
+    
+    ofImage debugImg;
+    
 };
